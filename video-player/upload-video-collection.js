a38f7afbc1071f7dbd228c3d4bd15e8d4d17a457
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-02-02 09:29:03 -0800

    fix async typo in video upload

--- video-player/upload-video-collection.js
+++ video-player/upload-video-collection.js
@@ -11,7 +11,7 @@ async function uploadVideoFile (filename) {
       overwrite: true,
       invalidate: true,
       resource_type: 'video',
-      aysnc: false,
+      async: false,
       eager: {
         crop: 'fill',
         width: 300,
