406bc5f51a22e1626e673cc8cde36f4b5757d0f6
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-07-08 09:38:32 -0700

    cleanup REAMDE and add comments

--- user-upload-workflow/remote-functions/README.md
+++ user-upload-workflow/remote-functions/README.md
@@ -1,23 +1,57 @@
 # Custom Fn
+For this example, we are writing a custom function using the imagemagick library. See [Cloudinary documentation](https://cloudinary.com/documentation/custom_functions#remote_functions) for more information.
+
+## Run this app locally
+```
+cd user-upload-workflow/remote-function
+npm i
+node app.js
+```
+The local app can be tested with cURL.
+
+In order to use a remote function with Cloudinary it must be available on the internet. The example app.js is deployed to Heroku. 
 
 ## Deploy to heroku
 
-* https://devcenter.heroku.com/articles/git
+
+Here are instructions for deploying this app to Heroku:
+[https://devcenter.heroku.com/articles/deploying-nodejs](https://devcenter.heroku.com/articles/deploying-nodejs)
+
+
+* [deploying with git](https://devcenter.heroku.com/articles/git)
 * heroku git:remote -a  secure-caverns-90265
 * Procfile
 * git push heroku master
 
 
-## Custom function to transform an image
+## Test locally with cURL
 
+```bash
+curl --location --request POST 'http://localhost:5000/api/file' \
+--form 'file=@"<path to local file"'
+```
+
+## Test remote call to Heroku app
+```
+curl --location --request POST 'https://secure-caverns-90265.herokuapp.com/api/file' \
+--form 'file=@"<path to local file>"' 
+```
+You'll might get a warning  like 
+```
+Warning: Binary output can mess up your terminal. Use "--output -" to tell 
+Warning: curl to output it to your terminal anyway, or consider "--output 
+Warning: <FILE>" to save to a file.
+```
+so you can add the `--output` flag to your cURL.
 
 ```
 curl --location --request POST 'https://secure-caverns-90265.herokuapp.com/api/file' \
---header 'Content-Type: multipart/form-data; boundary=--------------------------279813939285989578662257' \
---form 'name=@/Users/rebeccapeltz/training-images/cloudinary-training.jpg'
+--form 'file=@"/Users/rebeccapeltz/training-images/tiger-lilly.jpg"'
 ```
+
+
 ## Custom Function: remote function in transformation
+Older cached version
+
 https://res.cloudinary.com/cloudinary-training/image/upload/s--ZSceRCol--/fn_remote:aHR0cHM6Ly9zZWN1cmUtY2F2ZXJucy05MDI2NS5oZXJva3VhcHAuY29tL2FwaS9maWxl/shell
 
-## Fastly purge
-curl -X PURGE https://res.cloudinary.com/pictures77/image/upload/s--mLnimme6--/fn_remote:aHR0cHM6Ly9zZWN1cmUtY2F2ZXJucy05MDI2NS5oZXJva3VhcHAuY29tL2FwaS9maWxl/bo_6px_solid_coral/shell
--- user-upload-workflow/remote-functions/app.js
+++ user-upload-workflow/remote-functions/app.js
@@ -1,15 +1,24 @@
+// load libraries
+// use imagemagick for custome tranformation
 const im = require('imagemagick')
-const express = require('express')
-const multer = require('multer')
-const app = express()
+// use file system to store intermediate files 
+// for imagemagick transformation
 const fs = require('fs')
+const express = require('express')
+// parse multi part form
+const multer = require('multer')
+// use routing for API
+const app = express()
+// use moment for date used in overlay
 const moment = require('moment')
 
+// log and throw transformation error
 const fail = message => {
   console.log(message)
   throw new Error(message)
 }
 
+// process imagemagick transformation
 const perform = (operation, args) =>
   new Promise((resolve, reject) =>
     im[operation](args, (err, res) => {
@@ -22,6 +31,9 @@ const perform = (operation, args) =>
       }
     })
   )
+
+// cleanup after transformation
+// need to remove tempoarary files if they were created
 const postProcessResource = (resource, fn) => {
   let ret = null
   if (resource) {
@@ -36,6 +48,9 @@ const postProcessResource = (resource, fn) => {
   }
   return ret
 }
+
+// set up the transformation by calculating
+// date and created imagemagick args array
 const transform = async file => {
   // current time as string
   const date = moment().format('MMM Do YYYY, h:mm a')
@@ -48,20 +63,25 @@ const transform = async file => {
     '-draw',
     `text 5,15 'Date cached: ${date}'`
   ]
-  // prepare input and output files
+  // prepare temporary input and output files
   let inputFile = null
   let outputFile = null
   inputFile = '/tmp/inputFile.jpg'
+  // write the temporary input file
   fs.writeFileSync(inputFile, file.buffer)
-
+  // add the input file name to custom args
   customArgs.unshift(inputFile)
+  // add the output file name to custom args
   outputFile = '/tmp/outputFile.jpg'
   customArgs.push(outputFile)
-  // actual conversion
+
+  // call 'perform' function and then cleanup
   try {
     const output = await perform('convert', customArgs)
     postProcessResource(inputFile)
     if (outputFile) {
+      // if a transformation was successful there will
+      // be an output file and it can be returned
       return postProcessResource(outputFile, file =>
         Buffer.from(fs.readFileSync(file))
       )
@@ -73,15 +93,19 @@ const transform = async file => {
   }
 }
 
+// GET api to let use know where to POST
 app.get('/', function (req, res) {
   res.json({ message: 'use /api/file to post file for transform' })
 })
 
-// memory
+// set up multer using in memory storage
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
+// set up the API to process the transformation
 app.post('/api/file', upload.fields([{ name: 'file' }]), function (req, res) {
+  // call 'transform' to initiate the transformation
+  // and return a Base64 encoded image if successful
   return transform(req.files.file[0])
     .then(result => {
       res.statusCode = 200
@@ -102,8 +126,10 @@ app.post('/api/file', upload.fields([{ name: 'file' }]), function (req, res) {
       res.send(body)
     })
 })
+// set port
 app.set('port', process.env.PORT || 5000)
 
+// start server
 app.listen(app.get('port'), () =>
   console.log('App is running, server is listening on port ', app.get('port'))
 )
